c6c58fb4bdfd36cf460e1d6a605530ac9b46e0b9
Emile Nibasumba <2751024@collegelacite.ca>
2026-05-11 15:34:44 -0400

    Update app.py
    J'ajoute la fonction def affichage_application() pour qu'aucun élément de l'application ne soit affiché avant la saisie du vrai mot de passe.

--- app.py
+++ app.py
@@ -36,391 +36,394 @@ def verifier_mot_de_passe():
 if not verifier_mot_de_passe():
     st.stop()
 
-# =====================================================
-# TITRE
-# =====================================================
-
-st.title("🦟 Prédiction du Paludisme")
-st.sidebar.header("Paramètres")
-st.sidebar.markdown("---")
-
-st.write(
-    """
-    Prototype de prévision du paludisme
-    basé sur le Machine Learning.
-
-    Pays étudiés : le Burundi, le Rwanda et
-    la République Démocratique du Congo.
-
-    Réalisé par : Emile Nibasumba
-    """
-)
-
-# =====================================================
-# CARTE DES PAYS ÉTUDIÉS
-# =====================================================
-
-df_map = pd.DataFrame({
-    "Pays": [
-        "Rwanda",
-        "Burundi",
-        "RDC"
-    ],
+def afficher_application():
+    # =====================================================
+    # TITRE
+    # =====================================================
     
-    "lat": [
-        -1.94,
-        -3.37,
-        -2.87
-    ],
+    st.title("🦟 Prédiction du Paludisme")
+    st.sidebar.header("Paramètres")
+    st.sidebar.markdown("---")
     
-    "lon": [
-        29.87,
-        29.92,
-        23.65
-    ]
-})
-
-st.subheader("Pays étudiés")
-
-st.map(
-    df_map.rename(
-        columns={
-            "lat": "LATITUDE",
-            "lon": "LONGITUDE"
+    st.write(
+        """
+        Prototype de prévision du paludisme
+        basé sur le Machine Learning.
+    
+        Pays étudiés : le Burundi, le Rwanda et
+        la République Démocratique du Congo.
+    
+        Réalisé par : Emile Nibasumba
+        """
+    )
+    
+    # =====================================================
+    # CARTE DES PAYS ÉTUDIÉS
+    # =====================================================
+    
+    df_map = pd.DataFrame({
+        "Pays": [
+            "Rwanda",
+            "Burundi",
+            "RDC"
+        ],
+        
+        "lat": [
+            -1.94,
+            -3.37,
+            -2.87
+        ],
+        
+        "lon": [
+            29.87,
+            29.92,
+            23.65
+        ]
+    })
+    
+    st.subheader("Pays étudiés")
+    
+    st.map(
+        df_map.rename(
+            columns={
+                "lat": "LATITUDE",
+                "lon": "LONGITUDE"
+            }
+        )
+    )
+    
+    # =====================================================
+    # CHOIX DU PAYS
+    # =====================================================
+    
+    # =====================================================
+    # VALEURS RÉELLES 2022
+    # =====================================================
+    
+    valeurs_2022 = {
+    
+        "Rwanda": {
+            "incidence": 85.57,
+            "pluie": 4.55,
+            "temperature": 18.96
+        },
+    
+        "Burundi": {
+            "incidence": 342.31,
+            "pluie": 4.06,
+            "temperature": 20.72
+        },
+    
+        "Congo, Dem. Rep.": {
+            "incidence": 327.94,
+            "pluie": 5.46,
+            "temperature": 17.01
         }
-    )
-)
-
-# =====================================================
-# CHOIX DU PAYS
-# =====================================================
-
-# =====================================================
-# VALEURS RÉELLES 2022
-# =====================================================
-
-valeurs_2022 = {
-
-    "Rwanda": {
-        "incidence": 85.57,
-        "pluie": 4.55,
-        "temperature": 18.96
-    },
-
-    "Burundi": {
-        "incidence": 342.31,
-        "pluie": 4.06,
-        "temperature": 20.72
-    },
-
-    "Congo, Dem. Rep.": {
-        "incidence": 327.94,
-        "pluie": 5.46,
-        "temperature": 17.01
     }
-}
-
-pays = st.sidebar.selectbox(
-    "Choisir un pays",
-    [
-        "Burundi",
-        "Rwanda",
-        "Congo, Dem. Rep."
-    ]
-)
-
-# =====================================================
-# VALEURS PAR DÉFAUT SELON LE PAYS
-# =====================================================
-
-valeurs = valeurs_2022[pays]
-
-incidence_defaut = valeurs["incidence"]
-
-pluie_defaut = valeurs["pluie"]
-
-temperature_defaut = valeurs["temperature"]
-
-# =====================================================
-# CHARGER LE BON MODÈLE
-# =====================================================
-
-if pays == "Burundi":
     
-    model = joblib.load(
-        "Burundi_modele_paludisme.pkl"
+    pays = st.sidebar.selectbox(
+        "Choisir un pays",
+        [
+            "Burundi",
+            "Rwanda",
+            "Congo, Dem. Rep."
+        ]
     )
-
-elif pays == "Rwanda":
     
-    model = joblib.load(
-        "Rwanda_modele_paludisme.pkl"
-    )
-
-else:
+    # =====================================================
+    # VALEURS PAR DÉFAUT SELON LE PAYS
+    # =====================================================
     
-    model = joblib.load(
-        "Congo_Dem_Rep_modele_paludisme.pkl"
-    )
-
-# =====================================================
-# ENTRÉES UTILISATEUR
-# =====================================================
-
-st.sidebar.subheader("Variables d'entrée")
-
-st.info(
-    """
-    Informations importantes :
-
-    • Les précipitations représentent des moyennes exprimées en millimètres par jour (mm/jour).
-
-    • Les températures représentent des moyennes exprimées en degrés Celsius (°C).
-
-    • L'incidence du paludisme est exprimée en nombre de cas positifs pour 1000 habitants à risque.
-    """
-)
-
-annee_connue = st.sidebar.number_input(
-    "Dernière année connue",
-    min_value=2000,
-    value=2022
-)
-
-incidence_connue = st.sidebar.number_input(
-    "Dernière incidence connue",
-    value=incidence_defaut
-)
-
-annee_fin = st.sidebar.number_input(
-    "Année finale",
-    min_value=annee_connue + 1,
-    value=2030
-)
-
-pluie = st.sidebar.number_input(
-    "Pluie moyenne annuelle",
-    value=pluie_defaut
-)
-
-temperature = st.sidebar.number_input(
-    "Température moyenne annuelle",
-    value=temperature_defaut
-)
-
-# =====================================================
-# BOUTON PRÉDICTION
-# =====================================================
-
-if st.sidebar.button("Lancer la prédiction"):
+    valeurs = valeurs_2022[pays]
     
-    predictions = []
+    incidence_defaut = valeurs["incidence"]
     
-    annees = list(
-    range(
-        annee_connue + 1,
-        annee_fin + 1
-    )
-)
+    pluie_defaut = valeurs["pluie"]
+    
+    temperature_defaut = valeurs["temperature"]
+    
+    # =====================================================
+    # CHARGER LE BON MODÈLE
+    # =====================================================
+    
+    if pays == "Burundi":
         
-    incidence_prec = incidence_connue
+        model = joblib.load(
+            "Burundi_modele_paludisme.pkl"
+        )
     
-    # =================================================
-    # BOUCLE RÉCURSIVE
-    # =================================================
-    
-    for annee in annees:
+    elif pays == "Rwanda":
         
-        futur = pd.DataFrame({
+        model = joblib.load(
+            "Rwanda_modele_paludisme.pkl"
+        )
+    
+    else:
+        
+        model = joblib.load(
+            "Congo_Dem_Rep_modele_paludisme.pkl"
+        )
+    
+    # =====================================================
+    # ENTRÉES UTILISATEUR
+    # =====================================================
+    
+    st.sidebar.subheader("Variables d'entrée")
+    
+    st.info(
+        """
+        Informations importantes :
+    
+        • Les précipitations représentent des moyennes exprimées en millimètres par jour (mm/jour).
+    
+        • Les températures représentent des moyennes exprimées en degrés Celsius (°C).
+    
+        • L'incidence du paludisme est exprimée en nombre de cas positifs pour 1000 habitants à risque.
+        """
+    )
+    
+    annee_connue = st.sidebar.number_input(
+        "Dernière année connue",
+        min_value=2000,
+        value=2022
+    )
+    
+    incidence_connue = st.sidebar.number_input(
+        "Dernière incidence connue",
+        value=incidence_defaut
+    )
+    
+    annee_fin = st.sidebar.number_input(
+        "Année finale",
+        min_value=annee_connue + 1,
+        value=2030
+    )
+    
+    pluie = st.sidebar.number_input(
+        "Pluie moyenne annuelle",
+        value=pluie_defaut
+    )
+    
+    temperature = st.sidebar.number_input(
+        "Température moyenne annuelle",
+        value=temperature_defaut
+    )
+    
+    # =====================================================
+    # BOUTON PRÉDICTION
+    # =====================================================
+    
+    if st.sidebar.button("Lancer la prédiction"):
+        
+        predictions = []
+        
+        annees = list(
+        range(
+            annee_connue + 1,
+            annee_fin + 1
+        )
+    )
             
-            "annee": [annee],
+        incidence_prec = incidence_connue
+        
+        # =================================================
+        # BOUCLE RÉCURSIVE
+        # =================================================
+        
+        for annee in annees:
             
-            "pluie": [pluie],
+            futur = pd.DataFrame({
+                
+                "annee": [annee],
+                
+                "pluie": [pluie],
+                
+                "temperature": [temperature],
+                
+                "incidence_precedente": [
+                    incidence_prec
+                ]
+            })
             
-            "temperature": [temperature],
+            prediction = model.predict(
+                futur
+            )[0]
             
-            "incidence_precedente": [
-                incidence_prec
-            ]
+            predictions.append(prediction)
+            
+            # IMPORTANT :
+            # la prédiction devient
+            # l'incidence précédente suivante
+            
+            incidence_prec = prediction
+    
+        df_predictions = pd.DataFrame({
+            "Pays": [pays] * len(annees),
+            "Année": annees,
+            "Incidence prédite": predictions
         })
+    
+        date_export = datetime.now().strftime("%Y-%m-%d %H:%M:%S")         # création info sur la source et la date actuelle
+    
+        metadata = f"""Source : Application Streamlit - Prédiction du Paludisme        
+        Auteur : Emile Nibasumba
+        Modèle : Random Forest Regressor
+        Date export : {date_export}
+    
+        """
+    
+        csv = (metadata + df_predictions.to_csv(index=False)).encode("utf-8-sig")     # création fichier csv
+    
+        st.download_button(                                     # ajout bouton de téléchargement
+            label="📥 Télécharger les prévisions CSV",
+            data=csv,
+            file_name=f"predictions_{pays}.csv",
+            mime="text/csv"
+        )
         
-        prediction = model.predict(
-            futur
-        )[0]
+        # =================================================
+        # AFFICHAGE TEXTE
+        # =================================================
         
-        predictions.append(prediction)
+        st.header(f"Résultats des prévisions pour {pays}")
         
-        # IMPORTANT :
-        # la prédiction devient
-        # l'incidence précédente suivante
+        for annee, pred in zip(
+            annees,
+            predictions
+        ):
         
-        incidence_prec = prediction
-
-    df_predictions = pd.DataFrame({
-        "Pays": [pays] * len(annees),
-        "Année": annees,
-        "Incidence prédite": predictions
-    })
-
-    date_export = datetime.now().strftime("%Y-%m-%d %H:%M:%S")         # création info sur la source et la date actuelle
-
-    metadata = f"""Source : Application Streamlit - Prédiction du Paludisme        
-    Auteur : Emile Nibasumba
-    Modèle : Random Forest Regressor
-    Date export : {date_export}
-
-    """
-
-    csv = (metadata + df_predictions.to_csv(index=False)).encode("utf-8-sig")     # création fichier csv
-
-    st.download_button(                                     # ajout bouton de téléchargement
-        label="📥 Télécharger les prévisions CSV",
-        data=csv,
-        file_name=f"predictions_{pays}.csv",
-        mime="text/csv"
-    )
-    
-    # =================================================
-    # AFFICHAGE TEXTE
-    # =================================================
-    
-    st.header(f"Résultats des prévisions pour {pays}")
-    
-    for annee, pred in zip(
-        annees,
-        predictions
-    ):
-    
-        st.metric(
-            label=f"Prévision {annee}",
-            value=f"{pred:.2f}"
-    )
-    
-     
-# =====================================================
-# GRAPHIQUE DES PRÉVISIONS
-# =====================================================
-
-    df_graph = pd.DataFrame({
-        "Année": annees,
-        "Incidence": predictions,
-        "Type": "Prévisions futures"
-    })
-
-    fig = px.line(
-        df_graph,
-        x="Année",
-        y="Incidence",
-        color="Type",
-        markers=True,
-        title=f"Évolution prévisionnelle de l’incidence du paludisme au {pays}"
-    )
-
-    fig.update_layout(
-        xaxis_title="Année",
-        yaxis_title="Incidence",
-        template="plotly_dark"
-    )
-
-    st.plotly_chart(
-        fig,
-        use_container_width=True
-    )
-
+            st.metric(
+                label=f"Prévision {annee}",
+                value=f"{pred:.2f}"
+        )
+        
+         
     # =====================================================
-    # INTERPRÉTATION AUTOMATIQUE
+    # GRAPHIQUE DES PRÉVISIONS
     # =====================================================
-
-    premiere = predictions[0]
-
-    derniere = predictions[-1]
-
-    difference = derniere - premiere
-
-    # =====================================================
-    # ANALYSE
-    # =====================================================
-
-    if difference > 5:
     
-        interpretation = (
-            f"Le modèle prévoit une augmentation progressive "
-            f"de l'incidence du paludisme au {pays}."
+        df_graph = pd.DataFrame({
+            "Année": annees,
+            "Incidence": predictions,
+            "Type": "Prévisions futures"
+        })
+    
+        fig = px.line(
+            df_graph,
+            x="Année",
+            y="Incidence",
+            color="Type",
+            markers=True,
+            title=f"Évolution prévisionnelle de l’incidence du paludisme au {pays}"
+        )
+    
+        fig.update_layout(
+            xaxis_title="Année",
+            yaxis_title="Incidence",
+            template="plotly_dark"
+        )
+    
+        st.plotly_chart(
+            fig,
+            use_container_width=True
+        )
+    
+        # =====================================================
+        # INTERPRÉTATION AUTOMATIQUE
+        # =====================================================
+    
+        premiere = predictions[0]
+    
+        derniere = predictions[-1]
+    
+        difference = derniere - premiere
+    
+        # =====================================================
+        # ANALYSE
+        # =====================================================
+    
+        if difference > 5:
+        
+            interpretation = (
+                f"Le modèle prévoit une augmentation progressive "
+                f"de l'incidence du paludisme au {pays}."
+            )
+    
+        elif difference < -5:
+        
+            interpretation = (
+                f"Le modèle prévoit une diminution progressive "
+                f"de l'incidence du paludisme au {pays}."
+            )
+    
+        else:
+        
+            interpretation = (
+                f"Le modèle prévoit une stabilisation "
+                f"de l'incidence du paludisme au {pays}."
+            )
+    
+        # =====================================================
+        # AFFICHAGE
+        # =====================================================
+    
+        st.subheader("Interprétation automatique")
+    
+        st.success(interpretation)
+    
+        # =====================================================
+        # NIVEAU DE RISQUE
+        # =====================================================
+    
+        incidence_finale = predictions[-1]
+    
+        # =====================================================
+        # CLASSIFICATION OMS
+        # =====================================================
+    
+        if incidence_finale < 50:
+        
+            niveau = "Relativement faible"
+        
+            message = "🟢 Niveau de risque relativement faible"
+    
+        elif incidence_finale < 100:
+        
+            niveau = "Faible"
+        
+            message = "🟡 Niveau de risque faible"
+    
+        elif incidence_finale < 250:
+        
+            niveau = "Modéré"
+        
+            message = "🟠 Niveau de risque modéré"
+    
+        elif incidence_finale < 400:
+        
+            niveau = "Élevé"
+        
+            message = "🔴 Niveau de risque élevé"
+    
+        else:
+        
+            niveau = "Très élevé"
+        
+            message = "🚨 Niveau de risque très élevé"
+    
+        # =====================================================
+        # AFFICHAGE
+        # =====================================================
+    
+        st.subheader("Évaluation du niveau de risque")
+    
+        st.warning(
+            f"{message} "
+            f"({round(incidence_finale, 2)} cas / 1000 habitants à risque)"
+        )
+    
+        st.caption(
+            "Projet de science des données appliquées - Emile Nibasumba"
         )
 
-    elif difference < -5:
-    
-        interpretation = (
-            f"Le modèle prévoit une diminution progressive "
-            f"de l'incidence du paludisme au {pays}."
-        )
-
-    else:
-    
-        interpretation = (
-            f"Le modèle prévoit une stabilisation "
-            f"de l'incidence du paludisme au {pays}."
-        )
-
-    # =====================================================
-    # AFFICHAGE
-    # =====================================================
-
-    st.subheader("Interprétation automatique")
-
-    st.success(interpretation)
-
-    # =====================================================
-    # NIVEAU DE RISQUE
-    # =====================================================
-
-    incidence_finale = predictions[-1]
-
-    # =====================================================
-    # CLASSIFICATION OMS
-    # =====================================================
-
-    if incidence_finale < 50:
-    
-        niveau = "Relativement faible"
-    
-        message = "🟢 Niveau de risque relativement faible"
-
-    elif incidence_finale < 100:
-    
-        niveau = "Faible"
-    
-        message = "🟡 Niveau de risque faible"
-
-    elif incidence_finale < 250:
-    
-        niveau = "Modéré"
-    
-        message = "🟠 Niveau de risque modéré"
-
-    elif incidence_finale < 400:
-    
-        niveau = "Élevé"
-    
-        message = "🔴 Niveau de risque élevé"
-
-    else:
-    
-        niveau = "Très élevé"
-    
-        message = "🚨 Niveau de risque très élevé"
-
-    # =====================================================
-    # AFFICHAGE
-    # =====================================================
-
-    st.subheader("Évaluation du niveau de risque")
-
-    st.warning(
-        f"{message} "
-        f"({round(incidence_finale, 2)} cas / 1000 habitants à risque)"
-    )
-
-    st.caption(
-        "Projet de science des données appliquées - Emile Nibasumba"
-    )
+afficher_application()
